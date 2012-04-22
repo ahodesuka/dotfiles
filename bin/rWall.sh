daaedaf9cfd23224f13858973c5ae94ad3b2ad89
ahodesuka <mxcole90@gmail.com>
2012-04-22 14:35:11 -0500

    Added a few things.

--- bin/rWall.sh
+++ bin/rWall.sh
@@ -1,10 +1,30 @@
-#! /bin/bash
-WALLPAPERS="/mnt/dD/My Pictures/Backgrounds/mokou"
-ALIST=( `ls -w1 "$WALLPAPERS"` )
-RANGE=${#ALIST[@]}
-let "number = $RANDOM"
-let LASTNUM="`cat "$WALLPAPERS"/.last` + $number"
-let "number = $LASTNUM % $RANGE"
-echo $number > "$WALLPAPERS"/.last
-nitrogen --set-scaled --save "$WALLPAPERS/${ALIST[$number]}"
+#!/bin/bash
+
+STIME="60m"
+WALLDIR="$HOME/Pictures/Backgrounds/mokou"
+
+for pid in $(pgrep $(basename $0)); do
+    if [ "$pid" != "$$" ]; then
+        kill -TERM -$pid;
+    fi
+done
+
+if [ "$1" == "next" -a -f "/tmp/.rWall" ]; then
+    c=$(cat "/tmp/.rWall");
+fi
+
+while true; do
+    for n in "$WALLDIR"/*; do
+        if [ -n "$c" -a "$c" != "$n" ]; then
+            continue;
+        elif [ "$c" == "$n" ]; then
+            unset c;
+            continue;
+        fi
+        nitrogen --set-scaled --save "$n"
+        $(echo "$n" > "/tmp/.rWall")
+        sleep $STIME
+    done
+done
+
 exit
