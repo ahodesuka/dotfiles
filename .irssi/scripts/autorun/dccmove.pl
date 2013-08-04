be29106137708bb740f00e74a164ccbc925eb9e1
ahodesuka <ahoka@lavabit.com>
2013-08-03 22:57:54 -0500

    Remove irssi scripts that were not edited or created by myself.

---
+++ .irssi/scripts/autorun/dccmove.pl
@@ -0,0 +1,38 @@
+use strict;
+use Irssi;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = 20130728;
+%IRSSI = (
+    authors     => "ahoka",
+    contact     => "ahoka on irc.rizon.net",
+    name        => "dccmove.pl",
+    description => "Intelligently sets download location based on file name",
+);
+
+my $orig_down_path = Irssi::settings_get_str("dcc_download_path");
+
+sub on_dcc_request {
+    my ($dcc, $sendaddr) = @_;
+    my ($filename) = $dcc->{arg};
+    $filename =~ s/_/ /g;
+
+    if ($filename =~ m/^(?:\[.+\]\s)?(.+)(?:\s-\s.+)$/)
+    {
+        my $dir = Irssi::settings_get_str("dccmove_dir") . $1;
+
+        if (-d $dir)
+        {
+            Irssi::settings_set_str("dcc_download_path", $dir);
+            print "dccmove: saving " . $dcc->{arg} . " in " . $dir;
+        }
+    }
+}
+
+sub on_dcc_receive {
+    Irssi::settings_set_str("dcc_download_path", $orig_down_path);
+}
+
+Irssi::settings_add_str("dccmove", "dccmove_dir", "$ENV{HOME}/Anime/");
+Irssi::signal_add("dcc request", "on_dcc_request");
+Irssi::signal_add("dcc get receive", "on_dcc_receive");
