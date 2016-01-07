1acce43d6b2a51c29f243c478909c649716ef57d
ahoka <ahodesuka@users.noreply.github.com>
2016-01-06 19:27:57 -0600

    Update

--- .config/awesome/wallpaper.lua
+++ .config/awesome/wallpaper.lua
@@ -9,14 +9,7 @@ function wallpaper:create(args)
     setmetatable(instance,self)
     self.__index = self
 
-    local i = 1
-    instance.files = { }
-    args.dir = string.gsub(args.dir, " ", "\\ ")
-    local cmd = "find " .. args.dir .. " -type f -iregex '.*\.\\(png\\|jpg\\)'"
-    for f in io.popen(cmd):lines() do
-        instance.files[i] = f
-        i = i + 1
-    end
+    instance.dir = string.gsub(args.dir, " ", "\\ ")
 
     instance.timer = timer({ timeout = args.delay or 60 * 60 })
     instance.timer:connect_signal("timeout", function()
@@ -29,15 +22,24 @@ function wallpaper:create(args)
 end
 
 function wallpaper:new_wall()
+    self.files = { }
     local i = 1
+    local cmd = "find " .. self.dir .. " -type f -iregex '.*\.\\(png\\|jpg\\)'"
+    for f in io.popen(cmd):lines() do
+        self.files[i] = f
+        i = i + 1
+    end
+
+    i = 1
     if #self.files > 1 then
         i = math.random(1, #self.files)
-        if i == self.current_index then
+        if self.files[i] == self.current then
             i = i ~= 1 and i + 1 or i - 1
         end
     end
+
     gears.wallpaper.maximized(self.files[i])
-    self.current_index = i
+    self.current = self.files[i]
     self.timer:again()
 end
 
